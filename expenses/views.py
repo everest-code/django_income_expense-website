10cd59fdb1d0055fed93bf49e7c39fea0e6025a1
ChrisAchinga <achinga.chris@gmail.com>
2021-02-10 15:14:50 +0300

    fix;

--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 def  index(request):
-    return render(request, 'expenses/index.html')
+    items = Item.objects.all()
+    item_no = Item.objects.count()
+    context = {'items':items, 'item_no':item_no}
+    return render(request, 'expenses/index.html', context)
 
 def add_expense(request):
     return render(request, 'expenses/add_expense.html')
